3db774b561f44e1e625d609a51fc0acbe7d4c7c7
Nobodyishome1816 <tommcallister1816@gmail.com>
2025-01-15 09:31:42 +0000

    Initial commit
    connecting to the database

---
+++ frontend/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="styles.css">
+    <title>Hours Tracker</title>
+</head>
+<body>
+    <div id = "container">
+        <div class="navbar">
+
+        </div>
+        <h1>Good Morning</h1>
+    </div>
+</body>
+</html>
